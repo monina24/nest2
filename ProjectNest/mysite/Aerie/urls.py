5b2c4a5e90b9a78b55d5b9b86e36ec7ec9b1b94a
Zachary Kim <zkim2@illinois.edu>
2018-10-21 02:24:04 -0700

    more stuff

--- ProjectNest/mysite/Aerie/urls.py
+++ ProjectNest/mysite/Aerie/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path('createAccount', views.createAccount, name='createAccount'),
     path('welcome', views.welcome, name='welcome'),
     path('myNest', views.myNest, name='myNest'),
-    path('findNest', views.findNest, name='findNest'),
+    path('findNests', views.findNests, name='findNest'),
     path('createNest', views.createNest, name='createNest'),
 
     path('api/createAccount', views.createAccountAPI, name='createAcountAPI'),
     path('api/createNest', views.createNestAPI, name='createNestAPI'),
     path('api/login', views.loginAPI, name='loginAPI'),
     path('api/myNest', views.myNestAPI, name='myNestAPI'),
-    path('api/findNest', views.findNestAPI, name='findNestAPI')
+    path('api/findNests', views.findNestsAPI, name='findNestAPI')
 ]
